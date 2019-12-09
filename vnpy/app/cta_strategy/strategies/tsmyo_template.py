9d8eadf9f0d07be67f9373d3bdaeaff3261ce699
TheSuperMyo <18669719262@163.com>
2019-12-09 17:07:19 +0800

    自用策略开发模板

---
+++ vnpy/app/cta_strategy/strategies/tsmyo_template.py
@@ -0,0 +1,129 @@
+from datetime import time
+from vnpy.app.cta_strategy import (
+    CtaTemplate,
+    StopOrder,
+    TickData,
+    BarData,
+    TradeData,
+    OrderData,
+    BarGenerator,
+    ArrayManager
+)
+from vnpy.app.cta_strategy.base import (
+    EngineType,
+    STOPORDER_PREFIX,
+    StopOrder,
+    StopOrderStatus,
+)
+from vnpy.app.cta_strategy.TSMtools import TSMArrayManager
+
+class TSMyoTemplateStrategy(CtaTemplate):
+    """"""
+
+    author = "TheSuperMyo"
+
+    # 日内交易
+    exit_time = time(hour=14, minute=55)
+
+    # 针对不同交易时间的市场
+    open_time_night = time(hour=21,minute=0)# 商品夜盘
+    open_time_day_1 = time(hour=9,minute=0)# 商品
+    open_time_day_2 = time(hour=9,minute=30)# 股指
+
+    close_time_day = time(hour=15,minute=0)# 商品/股指（除了利率期货）
+    close_time_night_1 = time(hour=23,minute=0)# 其他夜盘商品
+    close_time_night_2 = time(hour=1,minute=0)# 工业金属
+    close_time_night_3 = time(hour=2,minute=30)# 黄金/白银/原油
+    
+    break_time_start_1 = time(hour=10,minute=15)# 商品茶歇
+    break_time_start_2 = time(hour=11,minute=30)# 全体午休
+    break_time_end_1 = time(hour=10,minute=30)# 商品茶歇
+    break_time_end_2 = time(hour=13,minute=0)# 股指下午
+    break_time_end_3 = time(hour=13,minute=30)# 商品下午
+
+    parameters = []
+    variables = []
+
+    def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
+        """"""
+        super(TSMyoTemplateStrategy, self).__init__(
+            cta_engine, strategy_name, vt_symbol, setting
+        )
+        self.bg = BarGenerator(self.on_bar)
+        self.am = TSMArrayManager()
+        # 策略自身订单管理
+        self.active_orderids = []
+
+    def on_init(self):
+        """
+        Callback when strategy is inited.
+        """
+        self.write_log("策略初始化")
+        # 根据需要的历史数据长度设定
+        self.load_bar(5)
+        
+    def on_start(self):
+        """
+        Callback when strategy is started.
+        """
+        self.write_log("策略启动")
+
+    def on_stop(self):
+        """
+        Callback when strategy is stopped.
+        """
+        self.write_log("策略停止")
+
+    def tick_filter(self, tick: TickData):
+        """
+        过滤异常时间的tick
+        """
+        pass
+
+    def on_tick(self, tick: TickData):
+        """
+        Callback of new tick data update.
+        """
+        self.bg.update_tick(tick)
+
+    def on_bar(self, bar: BarData):
+        """
+        Callback of new bar data update.
+        """
+
+    def on_order(self, order: OrderData):
+        """
+        Callback of new order data update.
+        """
+        # 移除已成交或已撤销的订单
+        if not order.is_active() and order.vt_orderid in self.active_orderids:
+            self.active_orderids.remove(order.vt_orderid)
+
+    def on_trade(self, trade: TradeData):
+        """
+        Callback of new trade data update.
+        """
+        # 邮寄提醒
+        self.send_email(f"{trade.vt_symbol}在{trade.time}成交，价格{trade.price}，方向{trade.direction}{trade.offset}，数量{trade.volume}")
+        self.put_event()
+
+    def on_stop_order(self, stop_order: StopOrder):
+        """
+        Callback of stop order update.
+        """
+        # 刚刚生成的本地停止单
+        if stop_order.status == StopOrderStatus.WAITING:
+            return
+        # 撤销的本地停止单，从活跃列表移除
+        if stop_order.status == StopOrderStatus.CANCELLED:
+            if stop_order.stop_orderid in self.active_orderids:
+                self.active_orderids.remove(stop_order.stop_orderid)
+        # 触发的本地停止单，停止单移除，限价单加入
+        if stop_order.status == StopOrderStatus.TRIGGERED:
+            if stop_order.stop_orderid in self.active_orderids:
+                self.active_orderids.remove(stop_order.stop_orderid)
+                self.active_orderids.extend(stop_order.vt_orderids)
+            # 撤掉其他停止单
+            for other_orderids in self.active_orderids:
+                if other_orderids.startswith(STOPORDER_PREFIX):
+                    self.cancel_order(other_orderids)
