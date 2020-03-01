bd97886f6cf0bedbcdbe937f37464abeb1df8ea6
TheSuperMyo <18669719262@163.com>
2020-03-01 11:06:51 +0800

    回测并更新分形通道策略
    在IC17-19上无效

--- examples/vn_trader/run.py
+++ examples/vn_trader/run.py
@@ -33,9 +33,9 @@ from vnpy.gateway.ctp import CtpGateway
 # from vnpy.gateway.gateios import GateiosGateway
 # from vnpy.gateway.bybit import BybitGateway
 # from vnpy.gateway.deribit import DeribitGateway
-from vnpy.gateway.uft import UftGateway
+# from vnpy.gateway.uft import UftGateway
 
-# from vnpy.app.cta_strategy import CtaStrategyApp
+from vnpy.app.cta_strategy import CtaStrategyApp
 # from vnpy.app.csv_loader import CsvLoaderApp
 # from vnpy.app.algo_trading import AlgoTradingApp
 # from vnpy.app.cta_backtester import CtaBacktesterApp
@@ -43,7 +43,7 @@ from vnpy.gateway.uft import UftGateway
 # from vnpy.app.risk_manager import RiskManagerApp
 # from vnpy.app.script_trader import ScriptTraderApp
 # from vnpy.app.rpc_service import RpcServiceApp
-# from vnpy.app.spread_trading import SpreadTradingApp
+from vnpy.app.spread_trading import SpreadTradingApp
 # from vnpy.app.portfolio_manager import PortfolioManagerApp
 from vnpy.app.option_master import OptionMasterApp
 # from vnpy.app.chart_wizard import ChartWizardApp
@@ -65,7 +65,7 @@ def main():
     # main_engine.add_gateway(SoptGateway)
     # main_engine.add_gateway(MinitestGateway)
     # main_engine.add_gateway(FemasGateway)
-    main_engine.add_gateway(UftGateway)
+    # main_engine.add_gateway(UftGateway)
     # main_engine.add_gateway(IbGateway)
     # main_engine.add_gateway(FutuGateway)
     # main_engine.add_gateway(BitmexGateway)
@@ -89,15 +89,15 @@ def main():
     # main_engine.add_gateway(BybitGateway)
     # main_engine.add_gateway(DeribitGateway)
 
-    # main_engine.add_app(CtaStrategyApp)
+    main_engine.add_app(CtaStrategyApp)
     # main_engine.add_app(CtaBacktesterApp)
     # main_engine.add_app(CsvLoaderApp)
     # main_engine.add_app(AlgoTradingApp)
     # main_engine.add_app(DataRecorderApp)
     # main_engine.add_app(RiskManagerApp)
-    main_engine.add_app(ScriptTraderApp)
+    # main_engine.add_app(ScriptTraderApp)
     # main_engine.add_app(RpcServiceApp)
-    # main_engine.add_app(SpreadTradingApp)
+    main_engine.add_app(SpreadTradingApp)
     # main_engine.add_app(PortfolioManagerApp)
     main_engine.add_app(OptionMasterApp)
     # main_engine.add_app(ChartWizardApp)
